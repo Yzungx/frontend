86aa7d91c4278225b05a683883c295a6812deba5
Yzungx <iruna872001@gmail.com>
2022-11-27 14:42:32 +0700

    38

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,73 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import { getAllUser } from '../../services/userService';
+import './UserManage.scss'
 class UserManage extends Component {
-
-    state = {
-
+    // khoi tao state
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUsers: [],
+        }
     }
 
-    componentDidMount() {
+    async componentDidMount() {
+        let response = await getAllUser('All')
+        if (response && response.errCode === "0") {
+            this.setState({
+                arrUsers: response.users,
+            }, () => {
+                console.log('check state user sau khi ham chay xong: ', this.state.arrUsers);
 
+            })
+            console.log('check state user 1: ', this.state.arrUsers);
+        }
     }
 
 
     render() {
+        let arrUsers = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className="users-containerr">
+                <div className="title text-center">Manage</div>
+                <div className="user-table mt-3 mx-1">
+                    <table id="customers">
+                        {/* tbdody  */}
+                        <tbody>
+                            <tr>
+                                <th>Email</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Address</th>
+                                <th>Action</th>
+                            </tr>
+
+                            {/* Toán tử && sẽ kiểm tra toán hạng đầu tiên.
+ Nếu kết quả là truthy thì toán hạng sẽ được 
+ tính toán và kết quả trả về là giá trị của toán hạng thứ hai. 
+ Tuy nhiên nếu giá trị của toán hạng đầu tiên là falsy thì toán 
+ hạng thứ hai không bao giờ được tính toán, kết quả trả về là giá 
+ trị falsy của toán hạng đầu. */}
+                            {arrUsers && arrUsers.map((item, index) => {
+                                console.log('check map: ', item, index);
+                                return (
+                                    <tr>
+                                        <td>{item.email}</td>
+                                        <td>{item.firstName}</td>
+                                        <td>{item.lastName}</td>
+                                        <td>{item.address}</td>
+                                        <td>
+                                            <button className='btn-edit'><i className='fas fa-pencil-alt'></i></button>
+                                            <button className='btn-delete'><i className='fas fa-trash'></i></button>
+                                        </td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         );
     }
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,8 +1,17 @@
 import axios from "../axios"
 
+//check tren db login
 const handleLoginApi = (userEmail, userPassword) => {
     console.log({ email: userEmail, password: userPassword });
     return axios.post('/api/login', { email: userEmail, password: userPassword });
 }
+//get data tren db
+const getAllUser = (inputId) => {
+    //method get ~ query param
+    return axios.get(`/api/get-all-users?id=${inputId}`);
+}
 
-export { handleLoginApi }
+export {
+    handleLoginApi,
+    getAllUser
+}
